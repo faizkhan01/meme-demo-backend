90ceff03c4024058881961b91d4386b5181682d5
faizur-bs23 <Faizur.Khan@brainstation-23.com>
2021-09-06 21:20:09 +0600

    some changes done

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const fileRoutes = require('./routes/file-upload-routes');
+"use strict";
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fileRoutes = require("./routes/file-upload-routes");
 
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
-require('./database')();
+require("./database")();
 
 app.use(bodyParser.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api', fileRoutes.routes);
+app.get("/", (req, res) => {
+  res.send("Hello From The Meme Gallery Backend");
+});
 
-app.listen(port, () => console.log(`server is listening on url http://localhost:${port}`));
+app.use("/api", fileRoutes.routes);
+
+app.listen(port, () =>
+  console.log(`server is listening on url http://localhost:${port}`)
+);
